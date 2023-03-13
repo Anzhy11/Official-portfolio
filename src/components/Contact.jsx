47465f6030535fd87165117a608aa5f14b5faef7
Anzhy11 <harnzzy@gmail.com>
2023-03-13 15:45:26 +0100

    added emailJS

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,7 +27,33 @@ const Contact = () => {
   };
 
   const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
 
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: 'Ahmed',
+        from_email: form.email,
+        to_email: 'codemastery.web@gmail.com',
+        message: form.desc,
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setLoading(false);
+        alert('Thank you, I will get back to you as soon as possible.');
+        setForm({
+          name: '',
+          email: '',
+          desc: '',
+        })
+      }, (error) => {
+        setLoading(false)
+        alert('Something went wrong.')
+      })
   }
 
   return (
