ed8bfca66d7b975bbec8f651c3d4cd1c0e311748
Anzhy11 <harnzzy@gmail.com>
2023-03-13 08:59:54 +0100

    completed about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ import { SectionWrapper } from '../hoc';
 
 const ServiceCard = ({ index, title, icon }) => {
   return(
-    <Tilt className="xs:w-[250px] w-full">
+    <Tilt className="xs:w-[250px] w-full mx-auto">
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="green-pink-gradient w-full  p-[1px] rounded-[20px] shadow-card"
