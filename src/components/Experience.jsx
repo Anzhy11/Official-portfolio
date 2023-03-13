62552041754c37442499fdc004d02cd00d290986
Anzhy11 <harnzzy@gmail.com>
2023-03-13 16:09:08 +0100

    fixed som issues

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -51,7 +51,7 @@ const Experience = () => {
         <h2 className={styles.sectionHeadText}>Work Experience.</h2>
       </motion.div>
 
-      <div className="mt-2 flex flex-col">
+      <div className="mt-20 flex flex-col">
         <VerticalTimeline>
           {experiences.map((experience, index) => (
             <ExperienceCard key={index} experience={experience} />
